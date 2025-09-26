e8242584018a33d918ce8d2923f28b14da050d7c
ABDNerwat <abhinerwa@gmail.com>
2025-09-26 15:24:15 +0530

    Add firebase object

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,21 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  // ... and the rest of your keys
+  apiKey: "AIzaSyDsf-fd11Wetj2yF9MYfd850-yHU530Tqs",
+  authDomain: "flashcard-26035.firebaseapp.com",
+  projectId: "flashcard-26035",
+  storageBucket: "flashcard-26035.firebasestorage.app",
+  messagingSenderId: "696129293503",
+  appId: "1:696129293503:web:9cb762111476b5a1a74a0a",
+  measurementId: "G-VTM1K9BYNF"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
